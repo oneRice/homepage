40482f68aef43f90412a43b8d46d4f226fa0eb44
oneRice <onericeyifan@gmail.com>
2016-09-30 17:24:42 +0200

    Split content route from app component

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule }   from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home.component';
 import { NavComponent } from './nav.component';
+import { ContentComponent } from './content.component';
 import { FootComponent } from './foot.component';
 
 import { routing } from './app.route';
@@ -19,6 +20,7 @@ import { routing } from './app.route';
     AppComponent,
     HomeComponent,
     NavComponent,
+    ContentComponent,
     FootComponent
   ],
   providers: [
---
+++ app/content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-content',
+    templateUrl: 'html/content.component.html'
+})
+
+export class ContentComponent {
+
+}

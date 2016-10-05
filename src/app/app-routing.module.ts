3bb609ae972c963ff8d16dd8fc009b5cc8405cbf
oneRice <onericeyifan@gmail.com>
2016-10-05 16:29:03 +0200

    add 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 // Actual Component
 import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   {
@@ -15,10 +16,10 @@ const appRoutes: Routes = [
     component: HomeComponent
   },
   // not found page
-  // {
-  //   path: '**',
-  //   component: PageNotFoundComponent;
-  // }
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,15 @@ import { HomeComponent } from './home/home.component';
 
 // router
 import { routing } from './app-routing.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    PageNotFoundComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,

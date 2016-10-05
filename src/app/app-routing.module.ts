cf6dfbfba6ec1ffe6abf2bb2f081cd53004f7f43
oneRice <onericeyifan@gmail.com>
2016-10-05 14:15:26 +0200

    nav template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Actual Component
+import { HomeComponent } from './home/home.component';
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-  providers: []
-})
-export class HomepageRoutingModule { }
+const appRoutes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  // default page
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  // not found page
+  // {
+  //   path: '**',
+  //   component: PageNotFoundComponent;
+  // }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,30 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// third party module
 import { MdlModule } from 'angular2-mdl';
 
+// actual component
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+// router
+import { routing } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    MdlModule
+    MdlModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
